fb0959791314bafeef23049490f046807fe22fe4
kkh1178 <kalisha.hill@gmail.com>
2021-11-18 12:44:17 -0600

    minor change to the scoreboard component

--- src/component/ScoreBoard.js
+++ src/component/ScoreBoard.js
@@ -1,9 +1,7 @@
 const ScoreBoard = ({ score }) => {
     return (
         <div className="score-board">
-            <div className="test">
-                <h2 className="score">Score: {score}</h2>
-            </div>
+            <h2 className="score">Score: {score}</h2>
         </div>
     )
 }
